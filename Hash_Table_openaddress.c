dd1df8a1d2b0f52d43d1b960d62cf29d8d1a2cc4
T.Horisawa <horisawa-taiki0930@outlook.com>
2023-03-10 22:22:28 +0900

    fix indent

--- Hash_Table_openaddress.c
+++ Hash_Table_openaddress.c
@@ -4,8 +4,8 @@
 
 struct set
 {
-  int key;
-  int data;
+	int key;
+	int data;
 };
 struct set *array;
 int capacity;	//init_arrayで定義
@@ -17,39 +17,39 @@ unsigned int hashFunction(int x)
 {
 	//MurmurHash
 	x ^= x >> 16;
-    x *= 0x7feb352d;
-    x ^= x >> 15;
-    x *= 0x846ca68b;
-    x ^= x >> 16;
+	x *= 0x7feb352d;
+	x ^= x >> 15;
+	x *= 0x846ca68b;
+	x ^= x >> 16;
 	return x % capacity;
 }
 int checkPrime(int n)
 {
-  int i;
-  if (n == 1 || n == 0)
-  {
-  return 0;
-  }
-  for (i = 2; i < n / 2; i++)
-  {
-  if (n % i == 0)
-  {
-    return 0;
-  }
-  }
-  return 1;
+	int i;
+	if (n == 1 || n == 0)
+	{
+		return 0;
+	}
+	for (i = 2; i < n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 int getPrime(int n)
 {
-  if (n % 2 == 0)
-  {
-  n++;
-  }
-  while (!checkPrime(n))
-  {
-  n += 2;
-  }
-  return n;
+	if (n % 2 == 0)
+	{
+		n++;
+	}
+	while (!checkPrime(n))
+	{
+		n += 2;
+	}
+	return n;
 }
 void init_array(int array_capacity)
 {
@@ -57,26 +57,26 @@ void init_array(int array_capacity)
 	array = (struct set *)malloc(capacity * sizeof(struct set));
 	for (int i = 0; i < capacity; i++)
 	{
-	array[i].key = 0;
-	array[i].data = 0;
+		array[i].key = 0;
+		array[i].data = 0;
 	}
 }
 void insert(int key, int data)
 {
-    int index = hashFunction(key);
-    if (array[index].data == 0)
-    {
-        array[index].key = key;
-        array[index].data = data;
-        size++;
-        // printf("\n Key (%d) has been inserted \n", key);
-    }
-      else if (array[index].key == key)
-    {
-      array[index].data = data;
-    }
-    else
-    {
+	int index = hashFunction(key);
+	if (array[index].data == 0)
+	{
+		array[index].key = key;
+		array[index].data = data;
+		size++;
+		// printf("\n Key (%d) has been inserted \n", key);
+	}
+	else if (array[index].key == key)
+	{
+		array[index].data = data;
+	}
+	else
+	{
 		//Open Address処理、10回で見つからなかったらCollision判定
 		pre_collision++;
 		for(int search_count = 1; search_count <= 10; search_count++)
@@ -96,115 +96,106 @@ void insert(int key, int data)
 				collision++;
 			}
 		}
-    }
+	}
 }
 void remove_element(int key)
 {
-  int index = hashFunction(key);
-  if (array[index].data == 0)
-  {
-  printf("\n This key does not exist \n");
-  }
-  else
-  {
-  array[index].key = 0;
-  array[index].data = 0;
-  size--;
-  printf("\n Key (%d) has been removed \n", key);
-  }
+	int index = hashFunction(key);
+	if (array[index].data == 0)
+	{
+		printf("\n This key does not exist \n");
+	}
+	else
+	{
+		array[index].key = 0;
+		array[index].data = 0;
+		size--;
+		printf("\n Key (%d) has been removed \n", key);
+	}
 }
-void display()
+void display(bool flag_print_EmptyArray)
 {
-  int i;
-  for (i = 0; i < capacity; i++)
-  {
-  if (array[i].data == 0)
-  {
-    // printf("\n array[%d]: / ", i);
-  }
-  else
-  {
-    printf("\n key: %4d array[%3d]: %d \t", array[i].key, i, array[i].data);
-  }
-  }
+	int i;
+	for (i = 0; i < capacity; i++)
+	{
+		if (array[i].data == 0 )
+		{
+			if(flag_print_EmptyArray == true)	printf("\n array[%d]: / ", i);
+		}
+		else
+		{
+			printf("\n key: %4d array[%3d]: %d \t", array[i].key, i, array[i].data);
+		}
+	}
 }
 int size_of_hashtable()
 {
-  return size;
+	return size;
 }
 int select_menu()
 {
-  int choice, key, data, n;
-  int c = 0;
-  init_array(10);
+	int choice, key, data, n;
+	int c = 0;
+	init_array(10);
 
-  do
-  {
-  printf("1.Insert item in the Hash Table"
-     "\n2.Remove item from the Hash Table"
-     "\n3.Check the size of Hash Table"
-     "\n4.Display a Hash Table"
-     "\n\n Please enter your choice: ");
+	do
+	{
+		printf("1.Insert item in the Hash Table"
+		"\n2.Remove item from the Hash Table"
+		"\n3.Check the size of Hash Table"
+		"\n4.Display a Hash Table"
+		"\n\n Please enter your choice: ");
 
-  scanf("%d", &choice);
-  switch (choice)
-  {
-  case 1:
+	scanf("%d", &choice);
+	switch (choice)
+	{
+		case 1:
+			printf("Enter key -:\t");
+			scanf("%d", &key);
+			printf("Enter data -:\t");
+			scanf("%d", &data);
+			insert(key, data);
+			break;
 
-    printf("Enter key -:\t");
-    scanf("%d", &key);
-    printf("Enter data -:\t");
-    scanf("%d", &data);
-    insert(key, data);
+		case 2:
+			printf("Enter the key to delete-:");
+			scanf("%d", &key);
+			remove_element(key);
+			break;
 
-    break;
+		case 3:
+			n = size_of_hashtable();
+			printf("Size of Hash Table is-:%d\n", n);
+			break;
 
-  case 2:
+		case 4:
+			display(false);
+			break;
 
-    printf("Enter the key to delete-:");
-    scanf("%d", &key);
-    remove_element(key);
+		default:
+			printf("Invalid Input\n");
+	}
 
-    break;
+	printf("\nDo you want to continue (press 1 for yes): ");
+	scanf("%d", &c);
 
-  case 3:
-
-    n = size_of_hashtable();
-    printf("Size of Hash Table is-:%d\n", n);
-
-    break;
-
-  case 4:
-
-    display();
-
-    break;
-
-  default:
-
-    printf("Invalid Input\n");
-  }
-
-  printf("\nDo you want to continue (press 1 for yes): ");
-  scanf("%d", &c);
-
-  } while (c == 1);
-  return 0;
+	} while (c == 1);
+	return 0;
 }
 
 int main()
 {
 	begin_time;
 
-	init_array(100000);
-	for(int num = 0; num < 10000; num++)
+	init_array(10000);
+	for(int num = 0; num < 1000; num++)
 	{
 		insert(num, num);
 	}
 
 	end_time;
 
-	// display();
+	display(false);
 	printf("\nsize:%4d, empty:%4d, capacity:%4d\n", size, capacity-size, capacity);
 	printf("Pre collision:%2d\n", pre_collision);
 	printf("Collision:%2d\n", collision);
