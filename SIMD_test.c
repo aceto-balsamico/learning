7d0da02450e6e64252ef2554b49f7566fc876ff3
T.Horisawa <horisawa-taiki0930@outlook.com>
2023-05-15 23:28:04 +0900

    add arm_neon intrinsic function

--- SIMD_test.c
+++ SIMD_test.c
@@ -65,12 +65,35 @@ uint32_t PopcntAlgorithm(u_int8_t* datax, u_int8_t* datay, size_t len)
 	uint32_t array_1count = 0;
 	for(int i = 0; i < len; i++)
 	{
-		data 			= datax[i] ^ datay[i];
+	 	data 			= datax[i] ^ datay[i];
 		data 			= (data & 0x55) + ((data & 0xAA) >> 1);
 		data 			= (data & 0x33) + ((data & 0xCC) >> 2);
 		array_1count	+= (data & 0x0F) + ((data & 0xF0) >> 4);
 	}
 	return array_1count;
+}
+
+uint32_t fbc_uint8_t_neon(uint8_t* arrayA, uint8_t* arrayB, size_t len)
+{
+	//16Byte(128bit)並列同時動作
+    size_t i;
+    uint32x4_t count = vdupq_n_u32(0);	//countはpopcountの総数を格納するSIMDレジスタ.vdupq_n_u32は、4つのuint32_t型の値を持つレジスタを作成し、すべての値を0に初期化
+    for (i = 0; i < len; i+=16) 
+	{
+        uint8x16_t vecA = vld1q_u8(arrayA + i);	//vld1q_u8は、uint8_t型の値を16個持つレジスタに読み込む
+        uint8x16_t vecB = vld1q_u8(arrayB + i);
+		uint8x16_t vecXOR = veorq_u8(vecA, vecB);
+		uint8x16_t popcount = vcntq_u8(vecXOR);
+		count = vaddq_u8(count, popcount);	//vaddq_u32は、2つのレジスタの値を足し合わせ
+    }
+	// printf("%u, %u, %u, %u\n", count[0], count[1], count[2], count[3]);
+	uint32_t result = 0;
+	for(int i = 0; i < 16; i++)
+	{
+		result += count[i];
+	}
+	
+    return result;
 }
 int main()
 {
@@ -101,11 +124,11 @@ int main()
 
 	begin_time(1);
 	for(int i = 0; i < 100000; i++)
-	result0 += __builtin_popcount(array32[i]);
+	result0 = __builtin_popcount(array32[i]);
 	end_time(1);
 	begin_time(1);
 	for(int i = 0; i < 100000; i++)
-	result1 += __builtin_popcount(aline_array32[i]);
+	result1 = __builtin_popcount(aline_array32[i]);
 	end_time(1);
 	begin_time(1);
 	for(int i = 0; i < 100000; i++)
@@ -114,6 +137,14 @@ int main()
 	begin_time(1);
 	for(int i = 0; i < 100000; i++)
 	result1 = PopcntAlgorithm(aline_array8, aline_array8_temp, 100000);
+	end_time(1);
+	begin_time(1);
+	for(int i = 0; i < 100000; i++)
+	result0 = fbc_uint8_t_neon(array8, array8_temp, 100000);
+	end_time(1);
+	begin_time(1);
+	for(int i = 0; i < 100000; i++)
+	result1 = fbc_uint8_t_neon(aline_array8, aline_array8_temp, 100000);
 	end_time(1);
 
 	printf("result0:%u, ,result1:%u\n", result0, result1);
@@ -124,39 +155,4 @@ int main()
 	free(aline_array8_temp);
 	free(aline_array32_temp);
 
-}
-
-//Does not work
-uint32_t fbc_uint8_t_neon(uint8_t* arrayA, uint8_t* arrayB, size_t len)
-{
-	//16Byte(128bit)並列同時動作
-    size_t i;
-    uint32x4_t count = vdupq_n_u32(0);	//countはpopcountの総数を格納するSIMDレジスタ.vdupq_n_u32は、4つのuint32_t型の値を持つレジスタを作成し、すべての値を0に初期化
-    for (i = 0; i < len / 16; i++) 
-	{
-        uint8x16_t vecA = vld1q_u8(arrayA + i * 16);	//vld1q_u8は、uint8_t型の値を16個持つレジスタに読み込む
-        uint8x16_t vecB = vld1q_u8(arrayB + i * 16);
-		uint8x16_t vecXOR = veorq_u8(vecA, vecB);
-		uint8x16_t popcount = vcntq_u8(vecXOR);
-        // uint32x4_t popcount = vcntq_u8(veorq_u8(vecA, vecB));	//veorq_u8は、2つのレジスタのビットごとの排他的論理和を計算.vcntq_u8は、レジスタのビットごとのpopcountを計算
-        count = vaddq_u8(count, popcount);	//vaddq_u32は、2つのレジスタの値を足し合わせ
-		// printf("count:%u %u %u %u\n", popcount[0], popcount[1], popcount[2], popcount[3]);
-		// uint32_t tempA = vaddvq_u8(vecA);printf("tempA:%u-", tempA);
-		// uint32_t tempB = vaddvq_u8(vecB);printf("tempB:%u-", tempB);
-		// uint32_t tempC = vaddvq_u32(count);printf("count:%u\n", tempC);
-	// uint8x16_t vecPopCount = vcntq_u8(vecXOR);
-	// for (int j = 0; j < 16; j++) {
-    // printf("%u ", vecPopCount[j]);
-	// }
-
-    }
-	//countはベクトルレジスタなので、単一整数変数に入れる必要がある。
-    uint32_t result = vaddvq_u32(count);	//vaddvq_u32は、レジスタの4つの値を足し合わせた結果をuint32_t型の値として返す
-    for (i = len - len % 16; i < len; i++) 
-	{
-		//lenが4の倍数でない残りの要素を処理するためのループ
-        result += __builtin_popcount(arrayA[i] ^ arrayB[i]);	//gccのビルトイン関数。CPU命令になることもあれば アルゴリズムで動くこともある。コンパイラ、CPU依存。
-    }
-	
-    return result;
 }
