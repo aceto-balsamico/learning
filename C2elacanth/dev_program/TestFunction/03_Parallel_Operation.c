c8a8b3574dd0d6389fda1d83639d3016c1701f9a
T.Horisawa <horisawa-taiki0930@outlook.com>
2024-12-04 22:12:18 +0900

    add Parallel Operation simulation

---
+++ C2elacanth/dev_program/TestFunction/03_Parallel_Operation.c
@@ -0,0 +1,115 @@
+#include "custom_common.h"
+
+
+#define NUM_MACHINES 4
+#define MAX_STEPS 5
+#define MAX_Parallel_Operation 4
+#define MAX_Execution_Count 2
+#define Interleaved_Rate 5
+
+typedef enum 
+{
+	READY,
+	BUSY,
+}Condition;
+
+// 機械の状態を管理する構造体
+typedef struct 
+{
+	char name;
+	int execution_count;
+	Condition condition;
+} Machine;
+
+void Interleaved_Operation(Machine machines[], int current_machine_index);
+void Operation_Function(Machine machines[], int current_machine_index);
+
+// 使用可能な機械のリストを作成し、その中からランダムに選択
+int select_random_machine(Machine machines[], int current_machine_index) 
+{
+	int available_machines[NUM_MACHINES - 1]; // 現在の機械以外を格納
+	int N_availble_machines = 0;
+
+	// 使用可能な機械をリストアップ
+	for (int i = 0; i < NUM_MACHINES; i++) 
+	{
+		if((i != current_machine_index) && (machines[i].condition == READY))
+		{
+			available_machines[N_availble_machines] = i;
+			N_availble_machines++;
+		}
+	}
+
+	// 使用中の機械がMax_Parallel_Operationを超える場合は-1を返す
+	if(NUM_MACHINES - N_availble_machines >= MAX_Parallel_Operation) {printf("---MAX_Parallel_Operation is over\n"); return -1;}
+	// 使用中の機械がMAX_Execution_Countを超える場合は-1を返す
+	int selected_machine = available_machines[rand() % N_availble_machines];
+	if(machines[selected_machine].execution_count >= MAX_Execution_Count) {printf("---[%c] is over MAX_Execution_Count\n", machines[selected_machine].name);return -1;}
+
+	// ランダムに1つの機械を選択
+	return selected_machine;
+}
+void Interleaved_Operation(Machine machines[], int current_machine_index) 
+{
+	// ランダムに別の機械を選択し、動作中でなければ再帰的に呼び出す
+	int next_machine_index = select_random_machine(machines, current_machine_index);
+	if(next_machine_index == -1) 
+	{
+		return;
+	}
+
+	machines[next_machine_index].condition = BUSY;
+	Operation_Function(machines, next_machine_index);
+	machines[next_machine_index].condition = READY;
+}
+
+// 機械のOperation関数
+void Operation_Function(Machine machines[], int current_machine_index) 
+{
+	Machine *current_machine = &machines[current_machine_index];
+	current_machine->execution_count++;
+
+	for(int i = 0; i < MAX_STEPS; i++)
+	{
+		if(current_machine->name == 'A'){/*  */}
+		else if(current_machine->name == 'B'){printf("\t");}
+		else if(current_machine->name == 'C'){printf("\t\t");}
+		else printf("\t\t\t");
+		printf("Machine %c: Operation step %d\n", current_machine->name, i);
+		
+		if (rand() % Interleaved_Rate == 0) 
+		{
+			// OperationFunctionを再帰的に呼び出す
+			Interleaved_Operation(machines, current_machine_index);
+		}
+	}
+}
+void Init_Machine(Machine machines[])
+{
+	for(int i = 0; i < NUM_MACHINES; i++)
+	{
+		machines[i].name = 'A' + i;
+		machines[i].execution_count = 0;
+		machines[i].condition = READY;
+	}
+}
+//@@@function
+void Parallel_Operation() 
+{
+	// 機械の初期化
+	Machine machines[NUM_MACHINES] = {};
+
+	srand(time(NULL)); // 乱数の初期化
+
+	// 最初の機械をランダムに選択して動作開始
+	for(int i = 0; i < 2; i++)
+	{
+		Init_Machine(machines);
+		printf("\n-----------------------START %d----------------------------\n", i);
+
+		int start_machine_index = rand() % NUM_MACHINES;
+		machines[start_machine_index].condition = BUSY;
+		Operation_Function(machines, start_machine_index);
+		machines[start_machine_index].condition = READY;
+	}
+}
