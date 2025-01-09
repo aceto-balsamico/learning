56ccb5b200e6ab2e9793347a7494681cc9d84d47
T.Horisawa <horisawa-taiki0930@outlook.com>
2025-01-09 22:21:30 +0900

    add func with var args

--- C2elacanth/dev_program/oasobi/08_typedef_functionptr.c
+++ C2elacanth/dev_program/oasobi/08_typedef_functionptr.c
@@ -4,23 +4,39 @@ typedef void (*FuncVoidNoArgs)(void);
 typedef int (*FuncIntNoArgs)(void);
 typedef void (*FuncVoidWithArgs)(int, float);
 typedef double (*FuncDoubleWithArgs)(int, double);
-typedef void (*FuncVoidWithPtrArg)(int*);
+typedef void (*FuncVoidWithPtrArg)(int *);
+typedef int (*FuncWithVarArgs)(const char*, ...);
 
 // 使用例
-void exampleFunction(void) {
-    printf("Hello, World!\n");
+void exampleFunction(void)
+{
+	printf("Hello, World!\n");
 }
-int getValue(void) {
-    return 42;
+int getValue(void)
+{
+	return 42;
 }
-void printValues(int a, float b) {
-    printf("a = %d, b = %f\n", a, b);
+void printValues(int a, float b)
+{
+	printf("a = %d, b = %f\n", a, b);
 }
-double multiply(int a, double b) {
-    return a * b;
+double multiply(int a, double b)
+{
+	return a * b;
 }
-void increment(int* value) {
-    (*value)++;
+
+int customPrintf(const char* format, ...) 
+{
+    va_list args;
+    va_start(args, format);
+    int result = vprintf(format, args);
+    va_end(args);
+    return result;
+}
+
+void increment(int *value)
+{
+	(*value)++;
 }
 
 //@@@function
@@ -30,14 +46,15 @@ void FunctionPtr()
 	FuncIntNoArgs func_i_n = getValue;
 	FuncVoidWithArgs func_v_if = printValues;
 	FuncDoubleWithArgs func_d_id = multiply;
-	FuncVoidWithPtrArg func_v_p = increment;
+	FuncWithVarArgs func_i_cp__ = customPrintf;
+	FuncVoidWithPtrArg func_v_cp = increment;
 
-	func_v_n(); // "Hello, World!" と出力
-	printf("%d\n", func_i_n()); // 42 と出力
-	func_v_if(5, 3.14); // "a = 5, b = 3.140000" と出力
+	func_v_n();						   // "Hello, World!" と出力
+	printf("%d\n", func_i_n());		   // 42 と出力
+	func_v_if(5, 3.14);				   // "a = 5, b = 3.140000" と出力
 	printf("%f\n", func_d_id(3, 4.5)); // 13.500000 と出力
+	func_i_cp__("Value: %d, %s\n", 42, "example"); // "Value: 42, example" と出力
 	int x = 10;
-	func_v_p(&x);
+	func_v_cp(&x);
 	printf("%d\n", x); // 11 と出力
 }
-
